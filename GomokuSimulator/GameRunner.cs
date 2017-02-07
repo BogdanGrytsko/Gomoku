a0bafa141b3cdb4fb50ae67fb778fb0aa09e91ae
BogdanGrytsko <bgrytsko@gmail.com>
2017-02-07 22:36:03 +0200

    Added GameRunner to measure time spent for moves.

---
+++ GomokuSimulator/GameRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using Gomoku2;
+
+namespace GomokuSimulator
+{
+    public class GameRunner
+    {
+        //todo create this via reflection;
+        private readonly Game game = new Game(15, 15);
+        private readonly Stopwatch timer = new Stopwatch();
+
+        public Cell DoMove()
+        {
+            timer.Start();
+            var playerMove = game.DoMove();
+            timer.Stop();
+            return playerMove;
+        }
+
+        public TimeSpan Elapsed { get { return timer.Elapsed; } }
+        public EstimatedBoard EstimatedBoard { get { return game.EstimatedBoard; } }
+
+        public void DoOpponentMove(int x, int y)
+        {
+            game.DoOpponentMove(x, y);
+        }
+
+        public bool HasFiveInARow(BoardCell cellType)
+        {
+            return game.HasFiveInARow(cellType);
+        }
+    }
+}
